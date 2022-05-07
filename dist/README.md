76769c22d4ca9796e86186abcf33d0b1e58415ac
egemenelz <egemenelz@gmail.com>
2022-05-06 20:38:41 -0400

    index.js is done

---
+++ dist/README.md
@@ -0,0 +1,40 @@
+# s
+
+  ## Description
+  s
+  
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  
+  ## Installation
+  s
+  
+  ## Usage
+  s
+  
+  ## Credits
+  s
+ 
+  
+  ## License
+  
+  
+  ## Badges
+  false
+  
+
+  ## Features
+  s
+  
+  
+  ## How to Contribute
+  s
+  
+  
+  ## Tests
+  s
+
--- index.js
+++ index.js
@@ -4,18 +4,19 @@ const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 
+// Questions includes the questions to ask user for the read me
 const questions = [
     'Your project Title (Required)',
     'What is the description of the project ? (Required)',
-    'Table of Contents (Optional)',
-    'What are steps required to install the project ?',
-    'Usage',
-    'Credits',
-    'License',
-    'Badges',
-    'Features',
+    'Table of Contents ',
+    'What are steps required to install the project ? (Required)',
+    'Can you provide instructions for use ? (Required)',
+    'Who did work with you in this project ?',
+    'Choose a License (Required)',
+    'Do you want to add custom badges ?',
+    'Features (Required)',
     'How to Contribute',
-    'Tests'
+    'How to test the application ?'
 ]
 
 
@@ -23,7 +24,7 @@ const promptProject = () => {
     return inquirer.prompt([
         {
             type: 'input',
-            name: 'name',
+            name: 'projectTitle',
             message: questions[0],
             validate: questionInput => {
                 if (questionInput) { return true; } else { console.log('Please enter your project title!'); return false; }
@@ -38,34 +39,92 @@ const promptProject = () => {
             }
         },
         {
+            type: 'input',
+            name: 'installation',
+            message: questions[3],
+            validate: questionInput => {
+                if (questionInput) { return true; } else { console.log('Please enter installation of the project!'); return false; }
+            }
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: questions[4],
+            validate: questionInput => {
+                if (questionInput) { return true; } else { console.log('Please enter usage of the project!'); return false; }
+            }
+        },
+        {
+            type: 'input',
+            name: 'credits',
+            message: questions[5],
+        },
+        {
+            type: 'checkbox',
+            name: 'license',
+            message: questions[6],
+            choices: ['GNU, General Public License v3.0', 'MIT License', 'ISC License']
+        },
+        {
+            type: 'confirm',
+            name: 'badges',
+            message: questions[7],
+            default: false
+        },
+        {
+            type: 'input',
+            name: 'features',
+            message: questions[8],
+            validate: questionInput => {
+                if (questionInput) { return true; } else { console.log('Please enter features of the project!'); return false; }
+            }
+        },
+        {
+            type: 'input',
+            name: 'contribute',
+            message: questions[9]
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: questions[10]
+        },
+        {
             type: 'checkbox',
             name: 'tableOfContents',
             message: questions[2],
-            choices: ['Installation', 'Usage', 'Credits', 'License', 'Badges', 'Features','How to Contribute', 'Test']
-        }
+            choices: ['Installation', 'Usage', 'Credits', 'License', 'Badges', 'Features', 'How to Contribute', 'Test']
+        },
     ])
 }
 
-promptProject()
-    .then(data => {
-        console.log(data)
-    })
+// Function to write README file
+
+function writeToFile(data) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./dist/README.md', data, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: 'README.md file created!'
+            });
+        });
+    });
+}
 
 
+// Function to initialize app
+function init() {
+    promptProject()
+        .then(data => {
+            return generateMarkdown(data);
+        })
+        .then(pageREADME => {
+            return writeToFile(pageREADME);
+        })
+}
 
-
-
-
-
-
-
-
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
-
-// TODO: Create a function to initialize app
-function init() { }
-
-// Function call to initialize app
 init();
